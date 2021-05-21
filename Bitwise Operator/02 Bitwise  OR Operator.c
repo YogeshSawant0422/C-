b38a9cb7a56129dd61ffc0b65550197e91767029
YogeshSawant0422 <yogeshsawant0422@gmail.com>
2021-05-21 23:09:20 +0530

    Bitwise Operator

---
+++ Bitwise Operator/02 Bitwise  OR Operator.c
@@ -0,0 +1,18 @@
+//Bitwise  OR Operator
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int iNo1 = 0 , iNo2 = 0 , iRes = 0;
+
+    printf("\n Enter Two Numbers : ");
+    scanf("%d%d",&iNo1,&iNo2);
+
+    iRes = iNo1 | iNo2;
+
+    printf("\n Output Of Bitwise OR Between >  %d | %d = %d",iNo1 , iNo2 , iRes);
+
+    getch();
+    return 0;
+}
